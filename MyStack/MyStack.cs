cfe76fafd483360de54060e8f47b83ad5ca490b2
Mike Westley <mike.westley@samaritanministries.org>
2022-10-19 09:09:54 -0500

    Make first test pass

---
+++ MyStack/MyStack.cs
@@ -0,0 +1,22 @@
+﻿namespace MyStack
+{
+    public class MyStack<T>
+    {
+        #region Members
+        private T[] stackArray;
+        private int maximumLength;
+        #endregion
+
+        #region Properties
+        public int Size { get; private set; }
+        #endregion
+
+        #region Constructor
+        public MyStack(int length)
+        {
+            maximumLength = length;
+            stackArray = new T[length];
+        }
+        #endregion
+    }
+}
