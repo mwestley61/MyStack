2f516a7f3d2157f6a929fcba224fbc32f5b2aeb8
Mike Westley <mike.westley@samaritanministries.org>
2022-10-19 08:54:03 -0500

    Write first failing test (no compile)

--- MyStackTests/MyStackTests.cs
+++ MyStackTests/MyStackTests.cs
@@ -5,6 +5,11 @@ namespace MyStackTests
     [TestFixture]
     public class MyStackTests
     {
-
+        [Test]
+        public void Creation()
+        {
+            MyStack<int> s = new MyStack<int>(3);
+            Assert.AreEqual(0, s.Size);
+        }
     }
 }
